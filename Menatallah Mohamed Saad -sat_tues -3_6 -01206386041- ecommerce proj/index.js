5ad305d20786115ceab925f8c56e29ddaf04ac19
menatallah mohamed <menatallahmohamedsaad@gmail.com>
2025-11-01 23:00:46 +0200

    Delete Menatallah Mohamed Saad -sat_tues -3_6 -01206386041- ecommerce proj directory

--- Menatallah Mohamed Saad -sat_tues -3_6 -01206386041- ecommerce proj/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import { initApp } from "./src/initApp.js";
-import nodemon from "nodemon";
-
-const app = express();
-
-initApp(app,express);
